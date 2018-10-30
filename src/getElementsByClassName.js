9b62fdf2c9af3b667c1e440509ce8b500ac978da
alex <alexliu12@gmail.com>
2018-10-29 20:29:42 -0700

    uploading to forked repo

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -3,8 +3,22 @@
 //   return document.getElementsByClassName(className);
 // };
 
+//You should use document.body, element.childNodes, and element.classList
+
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+	return recurse(className, [], document.body);
+};
+
+var recurse = function(className, classElements, myElement) {
+	for (var i in myElement.classList) {
+		if (myElement.classList[i] === className) {
+			classElements.push(myElement);
+			break;
+		}
+	}
+	myElement.childNodes.forEach(function(anElement) {
+		classElements.concat(recurse(className, classElements, anElement));
+	});
+	return classElements;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,75 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+	return recurseObj(obj, '');
 };
+
+var recurseObj = function(thingy, endString) {
+	
+	//checks if it is an object
+	if (typeof thingy === 'object' && !Array.isArray(thingy) && thingy !== null) {
+		return addObj(thingy, endString);
+
+	//checks if it's an array
+	} else if (Array.isArray(thingy)) {
+		return addArr(thingy, endString);
+
+	//everything else
+	} else {
+		endString += addThis(thingy, endString);
+	}
+
+	return endString;
+};
+
+//adds quotes for strings 
+var addThis = function(thingToAdd, endAdd) {
+	if (typeof thingToAdd === "string") {
+		endAdd += '"' + thingToAdd + '"';
+	} else {
+		endAdd += thingToAdd;
+	}
+	return endAdd;
+}
+
+var addObj = function(objToAdd, endObjStr) {
+	endObjStr += '{';
+	var tempObjStr = '';
+	Object.keys(objToAdd).forEach(function(key) {
+		if (typeof objToAdd[key] !== 'function' && typeof objToAdd[key] !== 'undefined') {
+			tempObjStr += recurseObj(key, '') + ':' + recurseObj(objToAdd[key], '') + ',';			
+		}
+	});
+	tempObjStr = tempObjStr.slice(0,-1);	//deletes last comma
+	endObjStr += tempObjStr + '}';
+	return endObjStr;
+}
+
+var addArr = function(arrToAdd, endArrStr) {
+	endArrStr += '['
+	var tempArrStr = '';
+	arrToAdd.forEach(function(ele) {
+		tempArrStr += recurseObj(ele, '') + ',';
+	});
+	tempArrStr = tempArrStr.slice(0,-1);	//deletes last comma
+	endArrStr += tempArrStr + ']';
+	return endArrStr;
+}
+
+//top level loop to go through all items in objects
+//CHECK if the item inside is an object, run THAT through a loop (recurse??)
+//if it is NOT an object, an array, then what
+//an array of objects? an object of arrays? 
+//if it is NOT an object or array we just add the item to the string. 
+//Simple??? 
+
+/*covers:
+	String
+	boolean
+	null
+	arrays
+	arrays inside arrays
+	objects
+	objects in objects
+	arrays in objects
+*/
